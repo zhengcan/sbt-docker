e59fea96ecb752a7c70ff2e7ffe21564d62f886b
Zheng Can <zc3593835@gmail.com>
2019-10-06 15:36:23 +0800

    update

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 ./update.sh
 
-docker build -f ./1.2.8/Dockerfile.8 -t zhengcan/sbt:8-1.2.8 .
-docker build -f ./1.2.8/Dockerfile.11 -t zhengcan/sbt:11-1.2.8 -t zhengcan/sbt:latest .
-docker build -f ./1.3.0/Dockerfile.8 -t zhengcan/sbt:8-1.3.0 .
-docker build -f ./1.3.0/Dockerfile.11 -t zhengcan/sbt:11-1.3.0 .
+docker build -f ./1.2/Dockerfile.8 -t zhengcan/sbt:8-1.2 .
+docker build -f ./1.2/Dockerfile.11 -t zhengcan/sbt:11-1.2 .
+docker build -f ./1.3/Dockerfile.8 -t zhengcan/sbt:8-1.3 -t zhengcan/sbt:latest .
+docker build -f ./1.3/Dockerfile.11 -t zhengcan/sbt:11-1.3 .
 
