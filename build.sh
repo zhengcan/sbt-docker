d6e592130f9883c87d4c9b714f26036a57852714
Zheng Can <zc3593835@gmail.com>
2019-10-06 16:26:24 +0800

    download sbt.tar if failed to add

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@
 
 ./update.sh
 
-docker build -f ./1.2/Dockerfile.8 -t zhengcan/sbt:8-1.2 .
-docker build -f ./1.2/Dockerfile.11 -t zhengcan/sbt:11-1.2 .
 docker build -f ./1.3/Dockerfile.8 -t zhengcan/sbt:8-1.3 -t zhengcan/sbt:latest .
 docker build -f ./1.3/Dockerfile.11 -t zhengcan/sbt:11-1.3 .
+
+docker build -f ./1.2/Dockerfile.8 -t zhengcan/sbt:8-1.2 .
+docker build -f ./1.2/Dockerfile.11 -t zhengcan/sbt:11-1.2 .
 
--- update.sh
+++ update.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# 1.2
-sed -e 's/FROM .*/FROM openjdk:8/g' -e 's/# Dockerfile Template #/# Auto Generated #/g' 1.2/Dockerfile > 1.2/Dockerfile.8
-sed -e 's/FROM .*/FROM openjdk:11/g' -e 's/# Dockerfile Template #/# Auto Generated #/g' 1.2/Dockerfile > 1.2/Dockerfile.11
-
 # 1.3
 sed -e 's/FROM .*/FROM openjdk:8/g' -e 's/# Dockerfile Template #/# Auto Generated #/g' 1.3/Dockerfile > 1.3/Dockerfile.8
 sed -e 's/FROM .*/FROM openjdk:11/g' -e 's/# Dockerfile Template #/# Auto Generated #/g' 1.3/Dockerfile > 1.3/Dockerfile.11
+
+# 1.2
+sed -e 's/FROM .*/FROM openjdk:8/g' -e 's/# Dockerfile Template #/# Auto Generated #/g' 1.2/Dockerfile > 1.2/Dockerfile.8
+sed -e 's/FROM .*/FROM openjdk:11/g' -e 's/# Dockerfile Template #/# Auto Generated #/g' 1.2/Dockerfile > 1.2/Dockerfile.11
 
 
 
